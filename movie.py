8b3d9304cf65458a0587d74b01b014540e5b0dc3
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-11 15:39:47 +0700

    Replace Conditional Logic with Polymorphism Step1

--- movie.py
+++ movie.py
@@ -18,6 +18,37 @@ class Movie:
     
     def get_title(self):
         return self.title
-    
+
+    def get_rental_points(self, days):
+        frequent_renter_points = 0
+        if self.get_price_code() == Movie.NEW_RELEASE:
+            # New release earns 1 point per day rented
+            frequent_renter_points += days
+        else:
+            # Other rentals get only 1 point
+            frequent_renter_points += 1
+
+        return frequent_renter_points
+
+    def get_price(self, days):
+        """Calculate the rental price."""
+        amount = 0
+        if self.get_price_code() == Movie.REGULAR:
+            # Two days for $2, additional days 1.50 per day.
+            amount = 2.0
+            if days > 2:
+                amount += 1.5 * (days - 2)
+        elif self.get_price_code() == Movie.CHILDRENS:
+            # Three days for $1.50, additional days 1.50 per day.
+            amount = 1.5
+            if days > 3:
+                amount += 1.5 * (days - 3)
+        elif self.get_price_code() == Movie.NEW_RELEASE:
+            # Straight $3 per day charge
+            amount = 3 * days
+
+        return amount
+
+
     def __str__(self):
         return self.title
