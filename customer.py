4a60604cbe69d2b33f83ac837fa0e03fbc97057a
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-11 15:08:34 +0700

    Extract method to compute_total_amount in customer.py

--- customer.py
+++ customer.py
@@ -18,27 +18,28 @@ class Customer:
         """Add a rental for this customer"""
         if rental not in self.rentals:
             self.rentals.append(rental)
-    
+
     def get_name(self):
         """Get the customer's name."""
         return self.name
-    
+
     def statement(self):
         """Create a statement of rentals for the current period.
 
-        Print all the rentals in the current period, 
+        Print all the rentals in the current period,
         along with total charges and frequent renter points.
-        
+
         Returns:
             the statement as a String
         """
-        total_amount = 0   # total rental charges
         # the .format method substitutes actual values into the fmt string
         statement = f"Rental Report for {self.name}\n\n"
         header_fmt = "{:40s}  {:6s} {:6s}\n"
         statement += header_fmt.format("Movie Title", "  Days", " Price")
         rental_fmt = "{:40s}  {:6d} {:6.2f}\n"
-        
+
+        total_frequent_renter_points = 0
+
         for rental in self.rentals:
             # compute the frequent renter points based on movie price code
             #  add a detail line to statement
@@ -46,13 +47,16 @@ class Customer:
                 rental.get_movie().get_title(),
                 rental.get_days_rented(),
                 rental.get_price())
-            # and accumulate activity
-            total_amount += rental.get_price()
+
+            total_frequent_renter_points += rental.rental_points()
 
         # footer: summary of charges
         statement += "\n"
         statement += "{:40s}  {:6s} {:6.2f}\n".format(
-                       "Total Charges", "", total_amount)
-        statement += "Frequent Renter Points earned: {}\n".format(rental.rental_points())
+                       "Total Charges", "", self.compute_total_amount())
+        statement += "Frequent Renter Points earned: {}\n".format(total_frequent_renter_points)
 
         return statement
+
+    def compute_total_amount(self):
+        return sum(rental.get_price() for rental in self.rentals)
