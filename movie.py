1ce7f1be24e87cfc32014e15621f4d400d041015
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-11 23:49:22 +0700

    Finished Replace Conditional Logic with Polymorphism

--- movie.py
+++ movie.py
@@ -1,54 +1,85 @@
+from abc import ABC, abstractmethod
+
+class PriceStrategy(ABC):
+    """Abstract base class for rental pricing."""
+
+    @abstractmethod
+    def get_price(self, days: int) -> float:
+        """The price of this movie rental."""
+        pass
+
+    @abstractmethod
+    def get_rental_points(self, days: int) -> int:
+        """The frequent renter points earned for this rental."""
+        pass
+
+
+class NewRelease(PriceStrategy):
+    """Pricing rules for New Release movies."""
+
+    def get_rental_points(self, days):
+        """New release rentals earn 1 point for each day rented."""
+        return days
+
+    def get_price(self, days):
+        return 3 * days
+
+
+class RegularPrice(PriceStrategy):
+
+    def get_rental_points(self, days):
+        """New release rentals earn 1 point for each day rented."""
+        return 1
+
+    def get_price(self, days):
+        amount = 2.0
+        if days > 2:
+            amount += 1.5 * (days - 2)
+        return amount
+
+class ChildrensPrice(PriceStrategy):
+    """Pricing rules for Children's movies."""
+
+    def get_rental_points(self, days: int) -> int:
+        return 1
+
+    def get_price(self, days: int) -> float:
+        amount = 1.5
+        if days > 3:
+            amount += 1.5 * (days - 3)
+        return amount
+
 class Movie:
     """
     A movie available for rent.
     """
-    # The types of movies (price_code). 
-    REGULAR = 0
-    NEW_RELEASE = 1
-    CHILDRENS = 2
-    
-    def __init__(self, title, price_code):
-        # Initialize a new movie. 
-        self.title = title
-        self.price_code = price_code
+    # The types of movies (price_code).
+    NEW_RELEASE = NewRelease()
+    REGULAR = RegularPrice()
+    CHILDRENS = ChildrensPrice()
 
-    def get_price_code(self):
-        # get the price code
-        return self.price_code
-    
+
+    def __init__(self, title, price_strategy):
+        # Initialize a new movie.
+        self.title = title
+        self.price_strategy = price_strategy
+
     def get_title(self):
         return self.title
 
-    def get_rental_points(self, days):
-        frequent_renter_points = 0
-        if self.get_price_code() == Movie.NEW_RELEASE:
-            # New release earns 1 point per day rented
-            frequent_renter_points += days
-        else:
-            # Other rentals get only 1 point
-            frequent_renter_points += 1
+    def get_price_code(self):
+        # get the price code
+        return self.price_strategy
 
-        return frequent_renter_points
+    def get_rental_points(self, days: int) -> float:
+        return self.price_strategy.get_rental_points(days)
 
-    def get_price(self, days):
+    def get_price(self, days :int) -> float:
         """Calculate the rental price."""
-        amount = 0
-        if self.get_price_code() == Movie.REGULAR:
-            # Two days for $2, additional days 1.50 per day.
-            amount = 2.0
-            if days > 2:
-                amount += 1.5 * (days - 2)
-        elif self.get_price_code() == Movie.CHILDRENS:
-            # Three days for $1.50, additional days 1.50 per day.
-            amount = 1.5
-            if days > 3:
-                amount += 1.5 * (days - 3)
-        elif self.get_price_code() == Movie.NEW_RELEASE:
-            # Straight $3 per day charge
-            amount = 3 * days
-
-        return amount
+        return self.price_strategy.get_price(days)
 
 
     def __str__(self):
         return self.title
+
+
