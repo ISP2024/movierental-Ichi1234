da6762b43ed97c3b217a3d53fc68894622c1d89a
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-10-18 22:29:52 +0700

    4. Define a Factory for Movies (MovieCatalog)

--- movie.py
+++ movie.py
@@ -1,4 +1,6 @@
-from pricing import PriceStrategy
+import csv
+
+from pricing import PriceStrategy, NewRelease
 from dataclasses import dataclass
 from typing import Collection
 
@@ -24,4 +26,30 @@ class Movie:
         """Return the moview title and published year."""
         return f"{self.title} ({self.year})"
 
+
+class MovieCatalog:
+
+    def __init__(self):
+        self.data = []
+        self.parse_csv()
+
+    def parse_csv(self):
+        with open("movies.csv", "r", encoding="UTF-8") as data_file:
+            reader = csv.reader(data_file)
+
+            for row in reader:
+                if row[0] == "#id":
+                    continue
+                self.data.append(Movie(row[1], NewRelease(), int(row[2]), row[3].split("|")))
+
+            data_file.close()
+
+    def get_movie(self, title, year=None):
+        for movie in self.data:
+            if title == movie.title and not year:
+                return movie
+            elif title == movie.title and year == movie.year:
+                return movie
+
+            return False
 
